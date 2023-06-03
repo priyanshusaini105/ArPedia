2414a2ecbc2d1c7b2a59ac8954afae88143e55fe
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-04 01:49:59 +0530

    Change Icon

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVRYZ1Bvdi8vLy8vQUFBQm1kWTlVVlVDTGszQmpmNzZiL1lXV3h5TFdtQlVEeTZFK2RWTHhCQ29KMFdZSFhBejhndmpLUG1Kc2tSOEIrbzNJQVNTb216L0llOWdQVmxJQ05uaERaSGJTWEFwV25ORWYyb3FNS0RFRG5FcUNUTnc1K05rK0RMcEZwRlFPNVhnZWlYenhMMWltemlzY01TRjlWRXI1RlNkUUVMdW5QdlZDZXc0OVY2MDA2ZnJmUU9LT09LenRjc3h3Q0VsQkNCMWNuSERYZWx0L3UwWXlUeU84RjdUWHdNK2ZyTzZ6QzBkYVN4eTJ5cnpLTmIwK1R2Q28rMWc2SkJBZ1E2VklwRzVMWVE3by9hb0VNRzFEMC91Zk1VNit3QjB3a05ZV1pDWEhnYUtOeGRDZHhvbFhidFhWV292R1FjWEtNdU8wbkZ0bEpzdUlqOFJaWjFRSFp3dHU3TjdJeXRPWmFJMy9GYmhJRnpCSXVyYll0dlE=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
