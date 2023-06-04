f50c7dd5839efb3b47dc7f6b14587d133e472c95
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-04 10:55:24 +0530

    Quite application if sceneindx is -ve

--- Assets/scripts/SwitchScene.cs
+++ Assets/scripts/SwitchScene.cs
@@ -13,10 +13,13 @@ public class SwitchScene : MonoBehaviour
     public void GoBack()
     {
         int previousSceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
+        if(previousSceneIndex >= 0)
         SceneManager.LoadScene(previousSceneIndex);
+        else
+        Application.Quit();
     } 
     void Update()
-{
+    {
     // Make sure user is on Android platform
     if (Application.platform == RuntimePlatform.Android) {
         
